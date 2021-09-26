344afa13b9ea7bacefcbeedff89f7b58c6ed161f
guanhuaxie <359488454@qq.com>
2021-09-26 19:42:15 +0800

    第一次提交

---
+++ src/pages/classify/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <view class="content">
+    分类
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello'
+    }
+  },
+  onLoad() {
+
+  },
+  methods: {}
+}
+</script>
+
+<style>
+/*.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin: 200rpx auto 50rpx auto;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}*/
+</style>
---
+++ src/pages/home/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <view class="content">
+    首页
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello'
+    }
+  },
+  onLoad() {
+
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss">
+.content{
+  background: $uni-color-primary;
+}
+</style>
